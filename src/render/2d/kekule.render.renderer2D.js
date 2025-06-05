b7d502bde784b703e3aced1d1581150553d562cf
Benjamin King <benking4@gmail.com>
2025-06-05 17:38:37 -0400

    fix

--- src/render/2d/kekule.render.renderer2D.js
+++ src/render/2d/kekule.render.renderer2D.js
@@ -2915,7 +2915,6 @@ Kekule.Render.ChemCtab2DRenderer = Class.create(Kekule.Render.Ctab2DRenderer,
 		//if (!this._needNodeDrawLabel(node))
 		//var autoCreateChargeAndRadicalMarker = nodeRenderOptions.autoCreateChargeAndRadicalMarker;
 		// TODO: charge and radical drawing are now handled togather, may be splitted in the future
-		var hasChargeOrRadical = node.getCharge() || node.getRadical();
 		// var needDrawCharge = (node.getCharge() && !node.fetchChargeMarker(false));
 		var needDrawCharge = (!!node.getCharge() && !node.fetchChargeMarker(false));
 		var needDrawRadical = (node.getRadical() && !node.fetchRadicalMarker(false));
